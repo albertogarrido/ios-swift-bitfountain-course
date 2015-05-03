d2768e7b031aa0bd7b4d20c51253a61ca9ea03de
albertogarrido <zgz.alberto@gmail.com>
2015-05-03 13:14:27 +0200

    finished 99 red balloons challenge

---
+++ challenge99RedBalloons/challenge99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  challenge99RedBalloons
+//
+//  Created by Alberto Garrido on 3/5/15.
+//  Copyright (c) 2015 Alberto Garrido. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage(named: "")
+}
--- challenge99RedBalloons/challenge99RedBalloons/ViewController.swift
+++ challenge99RedBalloons/challenge99RedBalloons/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var balloonImage: UIImageView!
+    
+    var balloonsArray:[Balloon] = []
+    let balloonsImages:[UIImage] = [
+        UIImage(named: "RedBalloon1.jpg")!,
+        UIImage(named: "RedBalloon2.jpg")!,
+        UIImage(named: "RedBalloon3.jpg")!,
+        UIImage(named: "RedBalloon4.jpg")!
+    ]
+    var currentBalloonCount = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var imgCount = 0
+        for(var balloonNum = 1; balloonNum < 100; balloonNum++ ){
+            var balloon = Balloon();
+            balloon.number = balloonNum
+            balloon.image = self.balloonsImages[imgCount]
+            imgCount = (imgCount == balloonsImages.count - 1) ? 0 : imgCount + 1;
+            self.balloonsArray.append(balloon);
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func nextBalloonPressed(sender: UIBarButtonItem) {
+        
+        self.currentBalloonCount++
+        let currentBalloon = self.balloonsArray[self.currentBalloonCount]
+        
+        UIView.transitionWithView(self.view, duration: 0.475, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            
+            self.informationLabel.text = "Ballon #\(currentBalloon.number)"
+            self.balloonImage.image = currentBalloon.image
+            
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+    }
 }
 
