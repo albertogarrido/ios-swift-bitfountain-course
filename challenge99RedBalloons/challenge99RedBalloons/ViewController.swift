5f6d0a495055d68869207e5c66e5b246200e518b
albertogarrido <zgz.alberto@gmail.com>
2015-05-03 12:27:28 +0200

    added 99 balloons challenge

---
+++ challenge99RedBalloons/challenge99RedBalloons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  challenge99RedBalloons
+//
+//  Created by Alberto Garrido on 3/5/15.
+//  Copyright (c) 2015 Alberto Garrido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ challenge99RedBalloons/challenge99RedBalloonsTests/challenge99RedBalloonsTests.swift
@@ -0,0 +1,36 @@
+//
+//  challenge99RedBalloonsTests.swift
+//  challenge99RedBalloonsTests
+//
+//  Created by Alberto Garrido on 3/5/15.
+//  Copyright (c) 2015 Alberto Garrido. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class challenge99RedBalloonsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
