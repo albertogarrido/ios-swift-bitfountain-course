4f74d31eaf8da26b17f43fd4d5ab0ec237d9d6ed
albertogarrido <zgz.alberto@gmail.com>
2015-05-03 17:44:21 +0200

    improve 99balloons challenge with prev button

--- challenge99RedBalloons/challenge99RedBalloons/ViewController.swift
+++ challenge99RedBalloons/challenge99RedBalloons/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var informationLabel: UILabel!
     @IBOutlet weak var balloonImage: UIImageView!
+    @IBOutlet weak var prevButton: UIBarButtonItem!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
     
     var balloonsArray:[Balloon] = []
     let balloonsImages:[UIImage] = [
@@ -21,38 +23,64 @@ class ViewController: UIViewController {
         UIImage(named: "RedBalloon4.jpg")!
     ]
     var currentBalloonCount = -1
+    let balloonsLimits = 99
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateBalloons(balloons: self.balloonsLimits);
+        self.prevButton.enabled = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func nextBalloonPressed(sender: UIBarButtonItem) {
+        self.currentBalloonCount++
+        showCurrentBalloon()
+        enableDisableButtons()
+    }
+    
+    @IBAction func prevBalloonPressed(sender: UIBarButtonItem) {
+        self.currentBalloonCount--
+        showCurrentBalloon()
+        enableDisableButtons()
+    }
+    
+    func enableDisableButtons(){
+        if(currentBalloonCount - 1 < 0){
+            self.prevButton.enabled = false
+        } else {
+            self.prevButton.enabled = true
+        }
         
+        if(currentBalloonCount + 1 >= balloonsLimits){
+            self.nextButton.enabled = false
+        } else {
+            self.nextButton.enabled = true
+        }
+
+    }
+    
+    func showCurrentBalloon(){
+        let currentBalloon = self.balloonsArray[self.currentBalloonCount]
+        UIView.transitionWithView(self.view, duration: 0.475, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            self.informationLabel.text = "Ballon #\(currentBalloon.number)"
+            self.balloonImage.image = currentBalloon.image
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+    }
+    
+    func generateBalloons(#balloons:Int){
         var imgCount = 0
-        for(var balloonNum = 1; balloonNum < 100; balloonNum++ ){
+        for(var balloonNum = 1; balloonNum <= balloonsLimits; balloonNum++ ){
             var balloon = Balloon();
             balloon.number = balloonNum
             balloon.image = self.balloonsImages[imgCount]
             imgCount = (imgCount == balloonsImages.count - 1) ? 0 : imgCount + 1;
             self.balloonsArray.append(balloon);
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func nextBalloonPressed(sender: UIBarButtonItem) {
-        
-        self.currentBalloonCount++
-        let currentBalloon = self.balloonsArray[self.currentBalloonCount]
-        
-        UIView.transitionWithView(self.view, duration: 0.475, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
-            
-            self.informationLabel.text = "Ballon #\(currentBalloon.number)"
-            self.balloonImage.image = currentBalloon.image
-            
-            }, completion: {
-                (finished: Bool) -> () in
-        })
     }
 }
 
